a3f34ac2084ff7160e3ab05336fbb8e42217e8fa
BlueBeret <63086194+BlueBeret@users.noreply.github.com>
2022-05-22 11:40:19 +0700

    update project yg java

--- Tugas Grafika 3D/src/App.java
+++ Tugas Grafika 3D/src/App.java
@@ -1,5 +1,61 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
+import javax.swing.JFrame;
+
+public class App implements GLEventListener{
+   private GLU glu = new GLU();
+   @Override
+   public void display( GLAutoDrawable drawable ) {   
+      final GL2 gl = drawable.getGL().getGL2();
+      gl.glTranslatef( 0f, 0f, -2.5f );
+      gl.glBegin( GL2.GL_LINES );
+      gl.glVertex3f( -0.75f,0f,0 );
+      gl.glVertex3f( 0f,-0.75f, 0 );
+      gl.glEnd();
+      //3d line
+      gl.glBegin( GL2.GL_LINES );
+      gl.glVertex3f( -0.75f,0f,3f );// 3 units into the window
+      gl.glVertex3f( 0f,-0.75f,3f );
+      gl.glEnd();
+   }
+   @Override
+   public void dispose( GLAutoDrawable arg0 ) {
+      //method body
+   }
+   @Override
+   public void init( GLAutoDrawable arg0 ) {
+      // method body
+   }
+   @Override
+   public void reshape( GLAutoDrawable drawable, int x, int y, int width, int height ) {
+      GL2 gl = drawable.getGL().getGL2();
+      if( height <=0 )
+         height =1;
+      final float h = ( float ) width / ( float ) height;
+      gl.glViewport( 0, 0, width, height );
+      gl.glMatrixMode( GL2.GL_PROJECTION );
+      gl.glLoadIdentity();
+      glu.gluPerspective( 45.0f, h, 1.0, 20.0 );
+      gl.glMatrixMode( GL2.GL_MODELVIEW );
+      gl.glLoadIdentity();
+   }
+   public static void main( String[] args ) {
+      //getting the capabilities object of GL2 profile
+      final GLProfile profile = GLProfile.get( GLProfile.GL2 );
+      GLCapabilities capabilities = new GLCapabilities( profile );
+      // The canvas 
+      final GLCanvas glcanvas = new GLCanvas( capabilities );
+      glcanvas.setSize( 400, 400 );
+      //creating frame
+      final JFrame frame = new JFrame (" 3d line");
+      //adding canvas to it
+      frame.getContentPane().add( glcanvas );
+      frame.setSize( frame.getContentPane().getPreferredSize() );
+      frame.setVisible(true);
+   }//end of main
+}//end of class
